55b02e63c12a3a1f555d28295d71c138012b9db5
Sophia Kalil <sophia.mkalil@gmail.com>
2025-04-24 00:04:29 -0300

    função remover_dado

--- funcoes.py
+++ funcoes.py
@@ -9,7 +9,7 @@ def rolar_dados(numero_dados):
     
     return lista_valores_dados
 
-def guardar_dado(lista_dados_rolados, lista_dados_estoque, indice):
+def guardar_dado(lista_dados_rolados, lista_dados_estoque, indice): #indice: indice da lista de dados rolados que deve ser passado para os dados estocados
     dados_rolados = []
     lista_rolados_estoque = []
     dado_armazenado = lista_dados_rolados[indice]
@@ -21,6 +21,21 @@ def guardar_dado(lista_dados_rolados, lista_dados_estoque, indice):
 
     lista_rolados_estoque.append(dados_rolados)
     lista_rolados_estoque.append(lista_dados_estoque)
+
+    return lista_rolados_estoque
+
+def remover_dado(lista_dados_rolados, lista_dados_estoque, indice): #indice: indice da lista de daodos estocados que deve ser removido e ir para a lista de daods rolados
+    lista_rolados_estoque = []
+    dados_estoque = []
+    dado_removido = lista_dados_estoque[indice]
+    lista_dados_rolados.append(dado_removido)
+
+    for i in range(len(lista_dados_estoque)):
+        if i != indice:
+            dados_estoque.append(lista_dados_estoque[i])
+
+    lista_rolados_estoque.append(lista_dados_rolados)
+    lista_rolados_estoque.append(dados_estoque)
 
     return lista_rolados_estoque
 
